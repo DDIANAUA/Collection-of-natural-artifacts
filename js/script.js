91cb3a4e5c3473ce1769d149063e85ec4bd536fa
DDIANAUA <dianatsapenko06@gmail.com>
2025-08-30 01:07:31 +0200

    aded new site

---
+++ js/script.js
@@ -0,0 +1,70 @@
+const swiper = new Swiper('.plants__swiper', {
+	slidesPerView: 1,
+	spaceBetween: 30,
+	pagination: {
+		el: '.swiper-pagination',
+	},
+	// pagination: {
+	// 	el: '.plants__number',
+	// 	type: 'fraction',
+	// 	renderFraction: function (currentClass, totalClass) {
+	// 		return (
+	// 			'<span class="' +
+	// 			currentClass +
+	// 			'"></span>' +
+	// 			' из ' +
+	// 			'<span class="' +
+	// 			totalClass +
+	// 			'"></span>'
+	// 		);
+	// 	},
+	// },
+	navigation: {
+		nextEl: '.plants__arrow--next',
+		prevEl: '.plants__arrow--back',
+	},
+	breakpoints: {
+		537: {
+			slidesPerView: 2,
+			pagination: {
+				el: '.swiper-pagination',
+			},
+		},
+		800: {
+			slidesPerView: 3,
+			pagination: {
+				el: '.plants__number',
+				type: 'fraction',
+				renderFraction: function (currentClass, totalClass) {
+					return (
+						'<span class="' +
+						currentClass +
+						'"></span>' +
+						' из ' +
+						'<span class="' +
+						totalClass +
+						'"></span>'
+					);
+				},
+			},
+		},
+		1000: {
+			slidesPerView: 4,
+			pagination: {
+				el: '.plants__number',
+				type: 'fraction',
+				renderFraction: function (currentClass, totalClass) {
+					return (
+						'<span class="' +
+						currentClass +
+						'"></span>' +
+						' из ' +
+						'<span class="' +
+						totalClass +
+						'"></span>'
+					);
+				},
+			},
+		},
+	},
+});
